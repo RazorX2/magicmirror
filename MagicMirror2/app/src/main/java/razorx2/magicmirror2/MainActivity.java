e1ae63f7405c07fcb3640b9ad0bf2644cf3fa38b
Vishal Tandale <2017vtandale@tjhsst.edu>
2016-07-21 10:32:20 -0400

    Facial Recognition is added

--- MagicMirror2/app/src/main/java/razorx2/magicmirror2/MainActivity.java
+++ MagicMirror2/app/src/main/java/razorx2/magicmirror2/MainActivity.java
@@ -4,21 +4,48 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import com.qualcomm.snapdragon.sdk.face.FacialProcessing;
+import com.qualcomm.snapdragon.sdk.face.FacialProcessing.FEATURE_LIST;
+import com.qualcomm.snapdragon.sdk.face.FacialProcessing.FP_MODES;
 
 import org.w3c.dom.Text;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.Timer;
 import java.util.TimerTask;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    private GridView gridView;
+    public static FacialProcessing faceObj;
+    public final String TAG = "FacialRecognitionActivity";
+    public final int confidence_value = 58;
+    public static boolean activityStartedOnce = false;
+    public static final String ALBUM_NAME = "serialize_deserialize";
+    public static final String HASH_NAME = "HashMap";
+    HashMap<String, String> hash;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
         final TextView Date = (TextView)findViewById(R.id.Date);
         final TextView Time = (TextView)findViewById(R.id.Time);
         Date.setText(getCurrentDate());
@@ -43,6 +70,7 @@ public class MainActivity extends AppCompatActivity {
         };
 
         t.start();
+
 
     }
     public String getCurrentDate(){
